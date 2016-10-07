1f227d771c7f01afc1587fe4cedb5f2005c2b1b3
GustavoDiogo <gustavo_guga_01@hotmail.com>
2016-10-07 13:50:17 -0300

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,48 +1,28 @@
 #include <stdio.h>
-#include <allegro.h>
- 
- 
- 
-int main(void)
-{
-   /* you should always do this at the start of Allegro programs */
-   if (allegro_init() != 0)
-      return 1;
- 
-   /* set up the keyboard handler */
-   install_keyboard(); 
- 
-   /* set a graphics mode sized 320x200 */
-   if (set_gfx_mode(GFX_AUTODETECT, 320, 200, 0, 0) != 0) {
-      if (set_gfx_mode(GFX_SAFE, 320, 200, 0, 0) != 0) {
-    set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
-    allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
-    return 1;
-      }
-   }
- 
-   /* set the color palette */
-   set_palette(desktop_palette);
- 
-   /* clear the screen to white */
-   clear_to_color(screen, makecol(255, 255, 255));
- 
-   /* you don't need to do this, but on some platforms (eg. Windows) things
-    * will be drawn more quickly if you always acquire the screen before
-    * trying to draw onto it.
-    */
-   acquire_screen();
- 
-   /* write some text to the screen with black letters and transparent background */
-   textout_centre_ex(screen, font, "Hello, world!", SCREEN_W/2, SCREEN_H/2, makecol(0,0,0), -1);
- 
-   /* you must always release bitmaps before calling any input functions */
-   release_screen();
- 
-   /* wait for a keypress */
-   readkey();
- 
-   return 0;
+#include <allegro5/allegro.h>
+
+int main(int argc, char **argv) {
+
+	ALLEGRO_DISPLAY *display = NULL;
+
+	if (!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+
+	display = al_create_display(640, 480);
+	if (!display) {
+		fprintf(stderr, "failed to create display!\n");
+		return -1;
+	}
+
+	al_clear_to_color(al_map_rgb(0, 120, 0));
+
+	al_flip_display();
+
+	al_rest(10.0);
+
+	al_destroy_display(display);
+
+	return 0;
 }
- 
-END_OF_MAIN()
