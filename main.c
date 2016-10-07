7de94288a6f2c3a949850fc2a07f5b0cb54bbd39
GustavoDiogo <gustavo_guga_01@hotmail.com>
2016-10-07 18:02:51 -0300

    alteraçao dos nomes

--- main.c
+++ main.c
@@ -1,99 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <allegro5/allegro.h>
 
-typedef struct Questions {
-    int answer_num;
-    char *main_question;
-    char *correct_answer;
-    char *answer2;
-    char *answer3;
-    char *answer4;
-    char *answer5;
-}Question;
+int main(int argc, char **argv) {
 
+	ALLEGRO_DISPLAY *display = NULL;
 
-int main()
-{
-    char q1[] = "Primeira pergunta aqui";
-    char ca[] = "resposta certa aqui";
-    char wa1[] = "Resposta errada 1";
-    char wa2[] = "Resposta errada 2";
-    char wa3[] = "Resposta errada 3";
+	if (!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
 
-    char newline[] = "\r\n";
-    size_t size = sizeof(newline) - sizeof(char);
+	display = al_create_display(640, 480);
+	if (!display) {
+		fprintf(stderr, "failed to create display!\n");
+		return -1;
+	}
 
-    FILE *questions_file = fopen("questions.txt", "wb");
+	al_clear_to_color(al_map_rgb(0, 120, 0));
 
-    fwrite("4", sizeof(char), 1, questions_file);
-    fwrite(newline, size, 1, questions_file);
-    fwrite(q1, sizeof(q1), 1, questions_file);
-    fwrite(newline, size, 1, questions_file);
-    fwrite(ca, sizeof(ca), 1, questions_file);
-    fwrite(newline, size, 1, questions_file);
-    fwrite(wa1, sizeof(wa1), 1, questions_file);
-    fwrite(newline, size, 1, questions_file);
-    fwrite(wa2, sizeof(wa2), 1, questions_file);
-    fwrite(newline, size, 1, questions_file);
-    fwrite(wa3, sizeof(wa3), 1, questions_file);
-    fwrite(newline, size, 1, questions_file);
+	al_flip_display();
 
-    fclose(questions_file);
+	al_rest(10.0);
 
-    free(questions_file);
-    questions_file = NULL;
+	al_destroy_display(display);
 
-    questions_file = fopen("questions.txt", "r");
-
-    char str[100];
-    fgets(str, 100, questions_file);
-
-    int num = str[0] - 48;
-    int x = 0;
-
-    Question new_question;
-    new_question.answer_num = num;
-
-    char qstr[100];
-    fgets(qstr, 100, questions_file);
-    new_question.main_question = qstr;
-
-    char astr1[100];
-    fgets(astr1, 100, questions_file);
-    new_question.correct_answer = astr1;
-
-    char astr2[100];
-    fgets(astr2, 100, questions_file);
-    new_question.answer2 = astr2;
-
-    if(num > 2) {
-
-        char astr3[100];
-        fgets(astr3, 100, questions_file);
-        new_question.answer3 = astr3;
-
-        if(num > 3) {
-
-            char astr4[100];
-            fgets(astr4, 100, questions_file);
-            new_question.answer4 = astr4;
-
-            if(num > 4){
-
-                char astr5[100];
-                fgets(astr5, 100, questions_file);
-                new_question.answer5 = astr5;
-            }
-        }
-    }
-
-    fclose(questions_file);
-
-    printf("%s\n", new_question.main_question);
-    printf("%s\n", new_question.correct_answer);
-    printf("%s\n", new_question.answer2);
-    printf("%s\n", new_question.answer3);
-    printf("%s\n", new_question.answer4);
-
-    return 0;
+	return 0;
 }
