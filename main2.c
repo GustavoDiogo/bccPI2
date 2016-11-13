893c5656c708cb131dc1d8286e11f884e4e32d02
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2016-11-13 16:37:57 -0200

    funçao da musica funfando

--- main2.c
+++ main2.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h> 
+#include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
+#include <stdbool.h>
 #define tracks 8
-void playlist(struct musicas);     
-struct musicas{
-   ALLEGRO_SAMPLE *musica[tracks];
-}; 
-int main(){
-
    ALLEGRO_DISPLAY *display = NULL;
+   ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+   ALLEGRO_TIMER *timer;
 
+struct Playlist{
+   ALLEGRO_SAMPLE *musica[tracks];
+};
+void play(struct Playlist *playlist){
+      int i;
+      i = rand() % 7;
+      printf("O número aleatório gerado foi %d\n",i);
+      al_play_sample(playlist->musica[i], 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_LOOP,NULL);
+
+}
+int iniciarjogo()
+{
    if(!al_init()){
       fprintf(stderr, "failed to initialize allegro!\n");
       return -1;
    }
- 
+   al_init_primitives_addon();
+   al_install_keyboard();
    if(!al_install_audio()){
       fprintf(stderr, "failed to initialize audio!\n");
       return -1;
@@ -30,19 +42,29 @@ int main(){
       fprintf(stderr, "failed to reserve samples!\n");
       return -1;
    }
-   
-   playlist1.musica[0] = al_load_sample( "musicas/batalha/track01.ogg" );
-   playlist1.musica[1] = al_load_sample( "musicas/batalha/track02.ogg" );
-   playlist1.musica[2] = al_load_sample( "musicas/batalha/track03.ogg" );
-   playlist1.musica[3] = al_load_sample( "musicas/batalha/track04.ogg" );
-   playlist1.musica[4] = al_load_sample( "musicas/batalha/track05.ogg" );
-   playlist1.musica[5] = al_load_sample( "musicas/batalha/track06.ogg" );
-   playlist1.musica[6] = al_load_sample( "musicas/batalha/track07.ogg" );
-   playlist1.musica[7] = al_load_sample( "musicas/batalha/track08.ogg" );
-  
+   event_queue = al_create_event_queue();
+   if(!event_queue){
+      printf("moios o event queue");
+      return -1;
+   }
+   timer = al_create_timer(1.0/60);
 
+}
+
+int main(){
+
+   iniciarjogo();
+
+   struct Playlist Batalha;
    
- //fodase
+   Batalha.musica[0] = al_load_sample( "musicas/batalha/track01.ogg" );
+   Batalha.musica[1] = al_load_sample( "musicas/batalha/track02.ogg" );
+   Batalha.musica[2] = al_load_sample( "musicas/batalha/track03.ogg" );
+   Batalha.musica[3] = al_load_sample( "musicas/batalha/track04.ogg" );
+   Batalha.musica[4] = al_load_sample( "musicas/batalha/track05.ogg" );
+   Batalha.musica[5] = al_load_sample( "musicas/batalha/track06.ogg" );
+   Batalha.musica[6] = al_load_sample( "musicas/batalha/track07.ogg" );
+   Batalha.musica[7] = al_load_sample( "musicas/batalha/track08.ogg" );
  
    display = al_create_display(640, 480);
  
@@ -54,13 +76,30 @@ int main(){
    /* Loop the sample until the display closes. */
 
 
-   int y = 0;
-   while(!y){
+   bool sair = false;
 
-   scanf("%d",&y);
-   playlist(musicas);
+   al_register_event_source(event_queue, al_get_timer_event_source(timer));
+   al_register_event_source(event_queue, al_get_keyboard_event_source());
+   al_start_timer(timer);
+   play(&Batalha);
 
- 
+   while(!sair){
+   ALLEGRO_EVENT ev;
+   al_wait_for_event(event_queue,&ev);
+   
+
+      if(ev.type == ALLEGRO_EVENT_KEY_DOWN){
+         switch(ev.keyboard.keycode)
+         {
+            case ALLEGRO_KEY_ESCAPE:
+            sair = true;
+            break;
+         }
+
+     }
+     else if(ev.type == ALLEGRO_EVENT_TIMER){
+      
+     }
    }
 
    
@@ -68,14 +107,8 @@ int main(){
    al_destroy_display(display);
    int i;
    for(i =0;i<8;i++){
-   al_destroy_sample(musicas[i]);
+   al_destroy_sample(Batalha.musica[i]);
    }
    
    return 0;
-}
-
-void playlist(struct musicas){
-      int i = rand() % 7;
-      al_play_sample(musicas[i], 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_ONCE,NULL);
-
 }
